42f9669a4fd94ef58f99a53d2069d18cf3654cb7
SammehFFXI <sammehffxi@gmail.com>
2017-01-02 12:31:54 -0600

    Synthall 1.0
    Simple addon that issues /lastsynth until you're out of materials or
    your inventory is full.

---
+++ SynthAll/README.md
@@ -0,0 +1,1 @@
+Simple Addon that issues /lastsynth until your inventory is full or you're out of materials and then stops.
---
+++ SynthAll/synthall.lua
@@ -0,0 +1,73 @@
+--[[
+Copyright © 2016, Sammeh of Quetzalcoatl
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of JobChange nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Sammeh BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
+
+
+
+_addon.name = 'synthall'
+_addon.author = 'Sammeh'
+_addon.version = '1.0'
+_addon.command = 'synthall'
+
+-- Script simply runs a /lastsynth until full or out of materials.
+
+
+require('chat')
+
+
+continue=0
+
+windower.register_event('addon command', function(...)
+	local args = T{...}
+    local cmd = args[1]
+	if cmd then 
+		if cmd:lower() == 'stop' then
+			continue = 0
+		end
+	else
+		continue = 1
+	end
+	synth_all()
+end)
+
+function synth_all()
+	while continue == 1 do
+		windower.send_command('input /lastsynth')
+		coroutine.sleep(23)
+	end
+end
+
+
+windower.register_event('incoming text', function(original, modified, original_mode, modified_mode, blocked)
+    if blocked or text == '' then
+        return
+    end
+
+	if original:strip_format() == "Synthesis canceled." or original:strip_format() == 'Unable to execute that command. Your inventory is full.' then
+		continue = 0
+	end
+end)

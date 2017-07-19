631c9cc397cb4b28f9484206705d6e1a00fad28a
Sam Bentrup <sam@ffspeed.com>
2017-07-19 18:46:41 -0500

    Jumper 1.0
    Kriss Kross will make you...

---
+++ Jumper/jumper.lua
@@ -0,0 +1,64 @@
+--[[
+Copyright © 2016, Sammeh of Quetzalcoatl
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Jumper nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Sammeh BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
+
+
+-- Kriss Kross will make you....
+
+
+_addon.name = 'jumper'
+_addon.author = 'Sammeh'
+_addon.version = '1.0'
+_addon.command = 'jumper'
+
+
+require('chat')
+
+
+continue=0
+
+windower.register_event('addon command', function(...)
+	local args = T{...}
+    local cmd = args[1]
+	if cmd then 
+		if cmd:lower() == 'stop' then
+			continue = 0
+		end
+	else
+		continue = 1
+	end
+	jumpjump()
+end)
+
+function jumpjump()
+	while continue == 1 do
+		windower.send_command('input /jump')
+		coroutine.sleep(2)
+	end
+end
+
+

31703cf64349b1ad83f1001dd22b256e5e33c471
Sam Bentrup <sam@ffspeed.com>
2017-08-06 10:04:46 -0500

    Pet charges 1.6
    Update:  Little more code efficient.  Also auto detects job points and
    merit categories.

--- PetCharges/petcharges.lua
+++ PetCharges/petcharges.lua
@@ -29,7 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name = 'PetCharges'
 _addon.author = 'Sammeh'
-_addon.version = '1.5'
+_addon.version = '1.6' 
 _addon.command = 'petcharges'
 
 config = require('config')
@@ -48,88 +48,91 @@ abilitytxt.flags = {}
 abilitytxt.flags.right = true
 
 settings = config.load(abilitytxt)
-
 abilities_list = texts.new('${value}', settings)
-
 showabilities = true
-
--- Variables for Merits and Job Points saving
-merits = 5
-jobpoints = 5
 
 equip_reduction = 0
 
 
 function displayabilities()
   if pet then
-	abilitylist = windower.ffxi.get_abilities().job_abilities
-	local list = "Charges: "..charges.." - "..next_ready_recast.."\n"
-  
-	for key,ability in pairs(abilitylist) do
-		ability_en = res.job_abilities[ability].en
-		ability_type = res.job_abilities[ability].type
-		ability_targets = res.job_abilities[ability].targets
-		ability_charges = res.job_abilities[ability].mp_cost
-		if ability_targets.Self == true and ability_type == 'Monster' then
-			if charges >= ability_charges then 
-				list = list..'\\cs(0,255,0)'..ability_en..'\\cs(255,255,255)'..'\n'
-			else
-				list = list..'\\cs(255,255,255)'..ability_en..'\n'
-			end
-		end
-	end
-	abilities_list.value = list
-	abilities_list:visible(true)
+    local list = "Charges: "..charges.." - "..next_ready_recast.."\n"
+    for key,ability in pairs(abilitylist) do
+        ability_en = res.job_abilities[ability].en
+        ability_type = res.job_abilities[ability].type
+        ability_targets = res.job_abilities[ability].targets
+        ability_charges = res.job_abilities[ability].mp_cost
+        if ability_targets.Self == true and ability_type == 'Monster' then
+            if charges >= ability_charges then 
+                list = list..'\\cs(0,255,0)'..ability_en..'\\cs(255,255,255)'..'\n'
+            else
+                list = list..'\\cs(255,255,255)'..ability_en..'\n'
+            end
+        end
+    end
+    abilities_list.value = list
+    abilities_list:visible(true)
   else
-	abilities_list:visible(false)
+    abilities_list:visible(false)
   end
 end
 
 windower.register_event('prerender', function()
-	local self = windower.ffxi.get_player()
-	pet = windower.ffxi.get_mob_by_target('pet')
-	if self then
-		if self.main_job == 'BST' then
-			duration = windower.ffxi.get_ability_recasts()[102]
-			if duration then 
-				chargebase = (30 - merits - jobpoints - equip_reduction)
-				charges = math.floor(((chargebase * 3) - duration) / chargebase)
-				next_ready_recast = math.fmod(duration,chargebase)
-				displayabilities()
-			end
-		end
-	end
+    if self then
+        if self.main_job == 'BST' then
+            duration = windower.ffxi.get_ability_recasts()[102]
+            if duration then 
+                chargebase = (30 - merits - jobpoints - equip_reduction)
+                charges = math.floor(((chargebase * 3) - duration) / chargebase)
+                next_ready_recast = math.fmod(duration,chargebase)
+                displayabilities()
+            end
+        end
+    end
 end)
 
 windower.register_event('outgoing chunk',function(id,data)
-	if id == 0x01A then
-		local packet = packets.parse('outgoing', data)
-		local ability_used = packet.Param
-		local category = packet.Category
-		if res.job_abilities[ability_used] then 
-			if res.job_abilities[ability_used].type == 'Monster' and category == 9 then
-				expect_ready_move = true
-			end
-		end
-	end
+    if id == 0x01A then
+        local packet = packets.parse('outgoing', data)
+        local ability_used = packet.Param
+        local category = packet.Category
+        if res.job_abilities[ability_used] then 
+            if res.job_abilities[ability_used].type == 'Monster' and category == 9 then
+                expect_ready_move = true
+            end
+        end
+    end
 end)
 
 windower.register_event('incoming chunk',function(id,data)
-	if id == 0x119 and expect_ready_move then
-		local gear = windower.ffxi.get_items()
-		local mainweapon = res.items[windower.ffxi.get_items(gear.equipment.main_bag, gear.equipment.main).id].en
-		local subweapon = res.items[windower.ffxi.get_items(gear.equipment.sub_bag, gear.equipment.sub).id].en
-		local legs = res.items[windower.ffxi.get_items(gear.equipment.legs_bag, gear.equipment.legs).id].en
-	
-		equip_reduction = 0
-		if mainweapon == "Charmer's Merlin" or subweapon == "Charmer's Merlin" then 
-			equip_reduction = equip_reduction + 5
-		end
-		if legs == "Desultor Tassets" then
-			equip_reduction = equip_reduction + 5
-		end
+    if id == 0x119 and expect_ready_move then
+        local gear = windower.ffxi.get_items()
+        local mainweapon = res.items[windower.ffxi.get_items(gear.equipment.main_bag, gear.equipment.main).id].en
+        local subweapon = res.items[windower.ffxi.get_items(gear.equipment.sub_bag, gear.equipment.sub).id].en
+        local legs = res.items[windower.ffxi.get_items(gear.equipment.legs_bag, gear.equipment.legs).id].en
+    
+        equip_reduction = 0
+        if mainweapon == "Charmer's Merlin" or subweapon == "Charmer's Merlin" then 
+            equip_reduction = equip_reduction + 5
+        end
+        if legs == "Desultor Tassets" then
+            equip_reduction = equip_reduction + 5
+        end
+        expect_ready_move = false
+    end
+	if id == 0x67 or id == 0x68 then
 		
-		expect_ready_move = false
+		local packet = packets.parse('incoming', data)
+		local msg_type = packet['Message Type']
+		pet_idx = packet['Pet Index']
+		if pet_idx ~= 0 and msg_type == 0x04 then 
+			pet = windower.ffxi.get_mob_by_target('pet')
+			abilitylist = windower.ffxi.get_abilities().job_abilities
+		else
+			if pet_idx == 0 then 
+				pet = nil
+			end
+		end
 	end
 end)
 
@@ -137,6 +140,34 @@ windower.register_event('addon command', function(command)
     if command == 'save' then
         config.save(settings, 'all')
     end
+	if command == 'jp' then
+	  for i,v in pairs(self.merits) do
+	    print (i,v)
+	  end
+	end
 end)
 
+windower.register_event('load', function()
+    if windower.ffxi.get_player() then 
+        coroutine.sleep(2) 
+        self = windower.ffxi.get_player()
+		if self.job_points.bst.jp_spent >= 100 then
+			jobpoints = 5
+		else
+			jobpoints = 0
+		end	
+    end
+	merits = self.merits.sic_recast
+end)
+
+windower.register_event('login', function()
+    coroutine.sleep(2)
+    self = windower.ffxi.get_player()
+	if self.job_points.bst.jp_spent >= 100 then
+		jobpoints = 5
+	else
+		jobpoints = 0
+	end	
+	merits = self.merits.sic_recast
+end)
 

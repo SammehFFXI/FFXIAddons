3fab41cb0821176e084d835e1cbf85b682669a39
Sam <sam@ffspeed.com>
2018-04-30 19:23:15 -0500

    StatueStatus - 1.0.0.2
    Updating to add in Red / Aurix statues

--- StatueStatus/StatueStatus.lua
+++ StatueStatus/StatueStatus.lua
@@ -1,20 +1,11 @@
 
 _addon.name = 'StatueStatus'
 _addon.author = 'Windower'
-_addon.version = '1.0.0.1'
+_addon.version = '1.0.0.2'
 _addon.command = 'StatueStatus'
 
 
 packets = require('packets')
-
-
---[[
-458784 green
-393248 blue
-262176 dark (transition)
-]]
-
-
 config = require('config')
 texts = require('texts')
 
@@ -33,9 +24,8 @@ statue_textbox = texts.new('${value}', settings)
 
 debug.setmetatable(nil, {__index = {}, __call = functions.empty})
 
-
-
 local statue_list = {}
+local AurixIndex = 0
 
 windower.register_event('incoming chunk', function(id,original,modified,injected,blocked)
     if id == 0x00E then
@@ -45,6 +35,11 @@ windower.register_event('incoming chunk', function(id,original,modified,injected
 		color = statue_list[index] or nil
 		if (status == 458784) then color = "Green" end
 		if (status == 393248) then color = "Blue" end
+		if (status == 327712) then color = "Red" end
+		if (color ~= "Green" and color ~= "Blue" and color ~= "Red") then 
+			color = "Aurix" 
+			AurixIndex = index
+		end
 		statue_list[index] = color
 	end
 end)
@@ -53,10 +48,16 @@ end)
 
 windower.register_event('prerender', function()
     local t = windower.ffxi.get_mob_by_index(windower.ffxi.get_player().target_index or 0)
-	if  statue_list[t.index] and (t.name == "Corporal Tombstone" or t.name == "Lithicthrower Image") then
+	if  statue_list[t.index] and (t.name == "Corporal Tombstone" or t.name == "Lithicthrower Image" or t.name == "Incarnation Icon" or t.name == "Impish Statue") then
 		statue_textbox.value = statue_list[t.index]
 	else
 		statue_textbox.value = nil
+	end
+	local mobArray = windower.ffxi.get_mob_array()
+	for (i,v) in pairs (mobArray) do
+		if i.valid_target and i.index == AurixIndex then
+			statue_textbox.value = statue_textbox.value.."\nAurix in Range"
+		end
 	end
 	statue_textbox:visible(t ~= nil)
 end)
